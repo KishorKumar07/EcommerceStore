40e1a97c9b1ca221708f310e4fadc9d49ec754d3
KishorKumar07 <86336555+KishorKumar07@users.noreply.github.com>
2023-12-04 12:05:06 +0530

    variables updated in server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ dotenv.config();
 //databse config
 connectDB();
 //esmodeule fix
-const _filename = fileURLToPath(import.meta.url);
-const _dirname=  path.dirname(_filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname=  path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -23,7 +23,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(_dirname, './client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
